59e98cc725f419615bb21762bbb1cdda2b5fd9f5
Manuel Alvarado <manuel.alvarado@bcr.gob.sv>
2024-01-18 16:38:10 -0600

    implementar cliente http y api getGifs()

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,8 +6,9 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
   private _tagsHistory: string[] = [];
-
-  constructor() {}
+  private apiKey: string = 'aLTSvEkZhdxxb1fgctTgf97eFH3QdD0A';
+  private urlBase: string = 'https://api.giphy.com/v1/gifs';
+  constructor(private http: HttpClient) {}
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -27,5 +29,17 @@ export class GifsService {
     if (tag.length === 0) return;
 
     this.organizateHistory(tag);
+    this.getGifs(tag);
+  }
+
+  public getGifs(search: string) {
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', 10)
+      .set('q', search);
+
+    this.http.get(`${this.urlBase}/search`, { params }).subscribe((resp) => {
+      console.log(resp);
+    });
   }
 }
