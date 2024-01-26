cb708844b7131e8d11213fdcad90db78708d52d7
Manuel Alvarado <manuel.alvarado@bcr.gob.sv>
2024-01-26 15:37:41 -0600

    Implementando Lazy-image en cardGif y estilos css

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+
+import { CardGifComponent } from './components/card-gif/card-gif.component';
+import { CardListComponent } from './components/card-list/card-list.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
-import { CardListComponent } from './components/card-list/card-list.component';
-import { CardGifComponent } from './components/card-gif/card-gif.component';
 
 @NgModule({
-  declarations: [HomePageComponent, SearchBoxComponent, CardListComponent, CardGifComponent],
-  imports: [CommonModule],
+  declarations: [
+    CardGifComponent,
+    CardListComponent,
+    HomePageComponent,
+    SearchBoxComponent,
+  ],
+  imports: [CommonModule, SharedModule],
   exports: [HomePageComponent],
 })
 export class GifsModule {}
